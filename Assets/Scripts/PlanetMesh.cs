d68a48be4655b79c9351c8281561c2334a4311c9
Cole Easton <ColeEaston941@gmail.com>
2020-04-24 21:16:09 -0400

    added first game functionality

--- Assets/Scripts/PlanetMesh.cs
+++ Assets/Scripts/PlanetMesh.cs
@@ -12,35 +12,53 @@ public class PlanetMesh : MonoBehaviour
 	private List<int> triangles = new List<int>();
 	private List<Vector3> vertices = new List<Vector3>();
 	private Dictionary<Int64, int> middlePointIndexCache;
+	private float perlinMag = 0.2f;
+	private int perlinOctaves = 10;
+	private int perlinInitLOD = 1;
+	private float perlinDecay = 2;
 	// Start is called before the first frame update
 	void Start()
     {
 		meshFilter = gameObject.GetComponent<MeshFilter>();
 		middlePointIndexCache = new Dictionary<long, int>();
 		meshTree = BuildIcosphere();
-		//meshTree.Refine(t => 3);
-		//meshTree.Refine(t => 7 - (int)(Vector3.Angle(t.GetCenter(meshTree.Vertices), Vector3.up)/30 + 0.5));
-		//meshTree.Refine(t => 1);
-		//meshTree.Refine(t => t.GetCenter(meshTree.Vertices).y > 0 ? 1 : 0);
-		//Refine(Vector3.up, 7);
-		//AddPerlin(.5f, 5);
+		meshTree.MinifyVertices();
+		meshFilter.mesh = meshTree.Mesh;
+		Bounds bounds = meshFilter.mesh.bounds;
+		bounds.extents = new Vector3((float)(transform.localScale.x * (1.01 + perlinMag)), (float)(transform.localScale.x * (1.01 + perlinMag)), (float)(transform.localScale.x * (1.01 + perlinMag)));
+		meshFilter.mesh.bounds = bounds;
 		meshFilter.mesh.RecalculateNormals();
-    }
+	}
 
     // Update is called once per frame
     void Update()
     {
-        if (true) //(Time.time % 0.1 - Time.deltaTime < 0)
-		{
-			double time = System.DateTime.Now.Second;
-			Refine(new Vector3(Mathf.Cos(Time.time * 3), Mathf.Sin(Time.time * 3), 0), 5);
-			meshTree.MinifyVertices();
-			meshFilter.mesh = meshTree.Mesh;
-			//AddPerlin(1f, 4);
-			meshFilter.mesh.RecalculateNormals();
-			Debug.Log("Time: " + (System.DateTime.Now.Second + 60 - time)%60);
-		}
+
     }
+
+	public void UpdateRefinement(Vector3 pos, int maxDegree = 7)
+	{
+		Refine(pos, maxDegree);
+		meshTree.MinifyVertices();
+		meshFilter.mesh = meshTree.Mesh;
+		AddPerlin();
+		meshFilter.mesh.RecalculateNormals();
+
+	}
+	public float GetRadius(Vector3 pos)
+	{
+		float sample = 1;
+		pos.Normalize();
+		for (int o = 0; o < perlinOctaves; o++)
+		{
+			sample += perlinMag * Mathf.Pow(perlinDecay, -o) * PerlinNoise.Noise(
+				pos.x * perlinInitLOD * Mathf.Pow(perlinDecay, o),
+				pos.y * perlinInitLOD * Mathf.Pow(perlinInitLOD, o),
+				pos.z * perlinInitLOD * Mathf.Pow(perlinDecay, o));
+		}
+		return (sample+1) * transform.localScale.x;
+	}
+
 
 	MeshTree BuildIcosphere()
 	{
@@ -93,7 +111,7 @@ public class PlanetMesh : MonoBehaviour
 		}
 		List<int> newTris = new List<int>(triangles.Count * 2);
 		
-		Vector3 q0 = targetDir * -1000, q1 = targetDir * 1000;
+		Vector3 q0 = targetDir * -100, q1 = targetDir * 100;
 		meshTree.Refine(delegate (Triangle t)
 		{
 			float v0 = signedVol(q0, meshTree.Vertices[t.Vertex0], meshTree.Vertices[t.Vertex1], meshTree.Vertices[t.Vertex2]);
@@ -108,11 +126,9 @@ public class PlanetMesh : MonoBehaviour
 			}
 			else
 			{
-				return (int)(maxDegree - Math.Pow(Vector3.Angle(targetDir, t.GetCenter(meshTree.Vertices))/180f, 0.5)*maxDegree + 0.5);
+				return (int)(-Math.Log(Vector3.Angle(targetDir, t.GetCenter(meshTree.Vertices))/180f, 2) + 0.5);
 			}
 		});
-		meshFilter.mesh = meshTree.Mesh;
-		meshFilter.mesh.RecalculateNormals();
 	}
 
 	private void addVertex(Vector3 p, bool normalize = true)
@@ -121,20 +137,22 @@ public class PlanetMesh : MonoBehaviour
 		vertices.Add(new Vector3(p.x / length, p.y / length, p.z / length));
 	}
 
-	void AddPerlin(float baseMagnitude, int numOctaves, float initialLOD = 1, float decayRatio = 2)
+	void AddPerlin(float initialLOD = 1, float decayRatio = 2)
 	{
+		Vector3[] vertices = meshFilter.mesh.vertices;
 		for (int i = 0; i < meshFilter.mesh.vertices.Length; i++)
 		{
 			float sample = 1;
-			for (int o = 0; o < numOctaves; o++)
+			for (int o = 0; o < perlinOctaves; o++)
 			{
-				sample += baseMagnitude * Mathf.Pow(decayRatio, -o) * PerlinNoise.Noise(
-					meshFilter.mesh.vertices[i].x * initialLOD * Mathf.Pow(decayRatio, o),
-					meshFilter.mesh.vertices[i].y * initialLOD * Mathf.Pow(decayRatio, o),
-					meshFilter.mesh.vertices[i].z * initialLOD * Mathf.Pow(decayRatio, o));
+				sample += perlinMag * Mathf.Pow(decayRatio, -o) * PerlinNoise.Noise(
+					vertices[i].normalized.x * initialLOD * Mathf.Pow(decayRatio, o),
+					vertices[i].normalized.y * initialLOD * Mathf.Pow(decayRatio, o),
+					vertices[i].normalized.z * initialLOD * Mathf.Pow(decayRatio, o));
 			}
-			meshFilter.mesh.vertices[i] = meshFilter.mesh.vertices[i] * (sample+1);
+			vertices[i] = vertices[i] * (sample+1);
 		}
+		meshFilter.mesh.vertices = vertices;
 		
 	}
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+	public PlanetMesh planet;
+	public float gravity = 1f;
+	public float MoveSpeed = 1;
+	public float LookSpeed = 30;
+	public float JumpForce = 20;
+	private Vector3 velocity;
+	// Start is called before the first frame update
+	void Start()
+    {
+		Vector3 up = transform.position - planet.transform.position;
+		transform.up = up;
+		planet.UpdateRefinement(up, 7);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 up = transform.position - planet.transform.position;
+		gameObject.transform.rotation = Quaternion.LookRotation(Quaternion.Euler(transform.right*90)*transform.up);
+		velocity -= gravity*up.normalized * Time.deltaTime;
+		transform.position += velocity;
+		float radius = planet.GetRadius(transform.position) + 5;
+		if (up.sqrMagnitude < radius*radius)
+		{
+			transform.position = transform.position.normalized * radius;
+			velocity = Vector3.zero;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 pos = transform.localPosition;
+			pos.z += MoveSpeed * Time.deltaTime;
+			transform.localPosition = pos;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 pos = transform.localPosition;
+			pos.z -= MoveSpeed * Time.deltaTime;
+			transform.localPosition = pos;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.localRotation *= Quaternion.Euler(-Vector3.up * LookSpeed * Time.deltaTime);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.localRotation *= Quaternion.Euler(Vector3.up * LookSpeed * Time.deltaTime);
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			velocity = up.normalized * JumpForce;
+		}
+	}
+}
